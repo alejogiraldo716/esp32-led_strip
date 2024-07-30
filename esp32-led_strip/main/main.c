91aa8f426bde4c70f040d71fc8b34b85737f8ea2
Alejo <alejo.giraldo716@gmail.com>
2024-07-30 14:45:30 -0500

    feat: Onboard LED functionality was added

--- esp32-led_strip/main/main.c
+++ esp32-led_strip/main/main.c
@@ -12,11 +12,34 @@
 #include "esp_chip_info.h"
 #include "esp_flash.h"
 #include "esp_system.h"
+#include <driver/gpio.h>
+#include "esp_log.h"
 
-void app_main(void)
+
+#define BLINK_GPIO 2
+#define CONFIG_BLINK_PERIOD 1000
+
+
+static const char *TAG = "example";
+static uint8_t s_led_state = 0;
+
+
+static void configure_led(void)
 {
-    printf("Hello world!\n");
+    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
+    gpio_reset_pin(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+}
 
+
+static void blink_led(void)
+{
+    /* Set the GPIO level according to the state (LOW or HIGH)*/
+    gpio_set_level(BLINK_GPIO, s_led_state);
+}
+
+void device_info_print(void){
     /* Print chip information */
     esp_chip_info_t chip_info;
     uint32_t flash_size;
@@ -32,21 +55,29 @@ void app_main(void)
     unsigned major_rev = chip_info.revision / 100;
     unsigned minor_rev = chip_info.revision % 100;
     printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
+    if (esp_flash_get_size(NULL, &flash_size) != ESP_OK)
+    {
         printf("Get flash size failed");
         return;
     }
 
     printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
     printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+}
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+void app_main(void)
+{
+    configure_led();
+    printf("This is an example!\n");
+    device_info_print();
+
+    while (1)
+    {
+        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
+        blink_led();
+        /* Toggle the LED state */
+        s_led_state = !s_led_state;
+        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
     }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
 }
